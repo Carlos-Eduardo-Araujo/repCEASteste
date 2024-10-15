abbdee25774d85eea63bb24c7b4c251d7c820198
Carlos-Eduardo-Araujo <carlos.eduardoarsilva@gmail.com>
2024-10-15 13:53:46 -0300

    calc_simples.py

--- calculadora_simples/calc_simples.py
+++ calculadora_simples/calc_simples.py
@@ -1,19 +1,15 @@
-# Função para somar dois números
 def somar(x, y):
     return x + y
 
 
-# Função para subtrair dois números
 def subtrair(x, y):
     return x - y
 
 
-# Função para multiplicar dois números
 def multiplicar(x, y):
     return x * y
 
 
-# Função para dividir dois números
 def dividir(x, y):
     if y != 0:
         return x / y
@@ -21,7 +17,6 @@ def dividir(x, y):
         return "Erro! Divisão por zero."
 
 
-# Função principal
 def calculadora():
     print("Selecione a operação:")
     print("1 - Somar")
@@ -29,14 +24,11 @@ def calculadora():
     print("3 - Multiplicar")
     print("4 - Dividir")
 
-    # Recebe a escolha do usuário
     escolha = input("Digite o número da operação desejada (1/2/3/4): ")
 
-    # Recebe os números para a operação
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
 
-    # Realiza a operação escolhida
     if escolha == '1':
         print(f"{num1} + {num2} = {somar(num1, num2)}")
     elif escolha == '2':
@@ -49,5 +41,4 @@ def calculadora():
         print("Opção inválida")
 
 
-# Executa a calculadora
 calculadora()
